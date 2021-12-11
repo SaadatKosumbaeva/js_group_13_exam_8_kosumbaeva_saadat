cd936b3a5e98892e7f4c8fbad381a749ad4ef0fd
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-12-11 12:40:47 +0600

    Created basic app structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteItemComponent } from './quotes/quote-item/quote-item.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NewQuoteComponent } from './new-quote/new-quote.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteItemComponent,
+    ToolbarComponent,
+    NewQuoteComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
